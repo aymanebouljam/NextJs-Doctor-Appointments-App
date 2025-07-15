edeb8e30b62d66acfdf0604f303c7eecf0e354bd
Aymane Bouljam <its.aymane.bouljam@gmail.com>
2025-07-15 01:58:57 +0100

    Fixed cards layout

--- frontend/app/(route)/details/[id]/_components/DoctorDetails.jsx
+++ frontend/app/(route)/details/[id]/_components/DoctorDetails.jsx
@@ -1,6 +1,6 @@
 import { GraduationCap, MapPin } from "lucide-react";
 import React from "react";
-import { PulseLoader } from "react-spinners";
+import { GridLoader, PulseLoader } from "react-spinners";
 import BookAppointment from "./BookAppointment";
 
 export default function DoctorDetails({ doctor }) {
@@ -11,7 +11,7 @@ export default function DoctorDetails({ doctor }) {
       <h1 className="font-bold text-xl col-span-full lg:mx-3 mx-auto p-4">
         Details
       </h1>
-      {doctor && (
+      {doctor ? (
         <div
           key={doctor?.id}
           href={`/details/${doctor?.doctorumentId}`}
@@ -20,7 +20,7 @@ export default function DoctorDetails({ doctor }) {
           <img
             src={baseURL + doctor?.image?.url}
             alt={doctor?.name}
-            className=" h-40 xl:h-56 object-contain"
+            className=" h-40 w-72 xl:h-56 object-contain"
           />
 
           {/* Content fills remaining space */}
@@ -38,13 +38,19 @@ export default function DoctorDetails({ doctor }) {
               {doctor?.category?.name}
             </h2>
 
-            <BookAppointment doctor = {doctor} />
+            <BookAppointment doctor={doctor} />
 
             <h2 className="font-bold text-xl mt-3">About</h2>
             <p className="text-gray-500">
               Specializing in {doctor?.category?.name}
             </p>
           </div>
+        </div>
+      ) : (
+        <div className="col-span-full flex flex-col mx-8 border rounded-lg p-8 gap-2 lg:flex-row lg:mx-3 xl:col-span-2 xl:h-max  hover:shadow-md animate-pulse">
+          <div className="w-16 h-16 bg-gray-300 rounded-full mx-auto" />
+          <div className="h-12 bg-gray-300 rounded w-3/4 mx-auto" />
+          <div className="h-12 bg-gray-300 rounded w-1/2 mx-auto" />
         </div>
       )}
     </>
--- frontend/app/(route)/details/[id]/_components/DoctorsSuggestion.jsx
+++ frontend/app/(route)/details/[id]/_components/DoctorsSuggestion.jsx
@@ -25,35 +25,47 @@ export default function DoctorsSuggestion() {
     <div className="px-8 lg:px-2 flex flex-col col-span-full xl:col-span-1">
       <h2 className="font-bold text-lg my-4">Suggestions</h2>
 
-      <div className="grid grid-cols-1 items-stretch gap-3 lg:grid-cols-3 xl:grid-cols-1">
-        {doctors.slice(0, 5).map((doc) => (
-          <Link
-            key={doc.id}
-            href={`/details/${doc?.documentId}`}
-            className="flex flex-col gap-3  xl:flex-row border rounded-lg items-center lg:hover:scale-105 transition-all ease-in-out w-full pt-2 lg:justify-center xl:p-4 xl:gap-3"
-          >
-            <Image
-              src={baseURL + doc?.image?.url}
-              alt={doc.name}
-              width={500}
-              height={300}
-              className="h-28 w-28 object-contain"
-            />
+      {doctors ? (
+        <div className="grid grid-cols-1 items-stretch gap-3 lg:grid-cols-3 xl:grid-cols-1">
+          {doctors.slice(0, 5).map((doc) => (
+            <Link
+              key={doc.id}
+              href={`/details/${doc?.documentId}`}
+              className="flex flex-col gap-3  xl:flex-row border rounded-lg items-center lg:hover:scale-105 transition-all ease-in-out w-full pt-2 lg:justify-center xl:p-4 xl:gap-3"
+            >
+              <Image
+                src={baseURL + doc?.image?.url}
+                alt={doc.name}
+                width={500}
+                height={300}
+                className="h-28 w-28 object-contain"
+              />
 
-            {/* Content fills remaining space */}
-            <div className="flex flex-col py-3  gap-2">
-              <h2 className="text-cyan-500 bg-cyan-100 rounded-full self-start px-3 py-2 font-semiBold text-sm">
-                {doc?.category?.name}
-              </h2>
+              {/* Content fills remaining space */}
+              <div className="flex flex-col py-3  gap-2">
+                <h2 className="text-cyan-500 bg-cyan-100 rounded-full self-start px-3 py-2 font-semiBold text-sm">
+                  {doc?.category?.name}
+                </h2>
 
-              <h4 className="font-bold text-sm">{doc?.name}</h4>
-              <p className="font-medium text-sm text-gray-600 mt-2">
-                {doc?.years_of_experience} Years of experience
-              </p>
+                <h4 className="font-bold text-sm">{doc?.name}</h4>
+                <p className="font-medium text-sm text-gray-600 mt-2">
+                  {doc?.years_of_experience} Years of experience
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 items-stretch gap-3 lg:grid-cols-3 xl:grid-cols-1">
+          {[0, 0, 0, 0].map((i) => (
+            <div className="flex flex-col gap-3  xl:flex-row border rounded-lg items-center lg:hover:scale-105 transition-all ease-in-out w-full pt-2 lg:justify-center xl:p-4 xl:gap-3">
+              <div className="w-16 h-16 bg-gray-300 rounded-full mx-auto" />
+              <div className="h-12 bg-gray-300 rounded w-3/4 mx-auto" />
+              <div className="h-12 bg-gray-300 rounded w-1/2 mx-auto" />
             </div>
-          </Link>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
